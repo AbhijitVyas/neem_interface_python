0f7c0bbdb4ea750d0c25bb65c845f8862142fd22
Abhijit Vyas <vyasabhijit21@gmail.com>
2023-01-25 13:38:09 +0100

    some more rest calls

--- src/rest_neem_interface/RESTClient.py
+++ src/rest_neem_interface/RESTClient.py
@@ -7,7 +7,6 @@ from .neemdata import NEEMData
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -25,9 +24,10 @@ def get_neem_to_load_into_kb():
     else:
         return jsonify(response), 400
 
+
 # not working at the moment
-#@app.route("/clear_kb")
-#def clear_knowledge_base():
+# @app.route("/clear_kb")
+# def clear_knowledge_base():
 #    response = NEEMData().clear_beliefstate()
 #    if response is not None:
 #        return jsonify("successfully wiped-out mongodb kb"), 200
@@ -52,7 +52,14 @@ def get_all_actions_start_time_stamps():
     else:
         return jsonify(response), 400
 
-
+@app.route("/get_all_actions_end_time")
+def get_all_actions_end_time_stamps():
+    response = NEEMData().get_all_actions_end_timestamps()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+    
 @app.route("/get_all_objects_participates_in_actions")
 def get_all_objects_participates_in_actions():
     response = NEEMData().get_all_objects_participates_in_actions()
@@ -62,9 +69,120 @@ def get_all_objects_participates_in_actions():
         return jsonify(response), 400
 
 
+# this method will not return any pose since knowrob tf_get_pose has some bug and does not return any value at the 
+#  moment 
 @app.route("/get_handpose_at_start_of_action")
 def get_handpose_at_start_of_action():
     response = NEEMData().get_handpose_at_start_of_action()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_source_container_while_grasping")
+def get_source_container_while_grasping():
+    response = NEEMData().get_source_container_while_grasping()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+# this method at the moment will return Null because of knowrob issue
+@app.route("/get_source_container_pose_while_grasping")
+def get_source_container_pose_while_grasping():
+    response = NEEMData().get_source_container_pose_while_grasping()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_all_objects_with_roles_participates_in_actions")
+def get_all_obj_participate_each_event():
+    response = NEEMData().get_all_obj_roles_which_participate_each_event()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_shape_for_source_container_objects")
+def get_shape_for_all_container_objects():
+    response = NEEMData().get_shape_for_source_container_objects()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_color_for_source_container_object")
+def get_color_for_all_container_objects():
+    response = NEEMData().get_color_for_source_container_objects()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_target_obj_for_pouring")
+def get_target_obj_for_pouring():
+    response = NEEMData().get_target_obj_for_pouring()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_pouring_side")
+def get_pouring_side_for_target_obj():
+    response = NEEMData().get_pouring_side()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_max_pouring_angle_for_source_obj")
+def get_max_pouring_angle_for_source_obj():
+    response = NEEMData().get_max_pouring_angle_for_source_obj()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_min_pouring_angle_for_source_obj")
+def get_min_pouring_angle_for_source_obj():
+    response = NEEMData().get_min_pouring_angle_for_source_obj()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_pouring_event_time_duration")
+def get_pouring_event_time_duration():
+    response = NEEMData().get_pouring_event_time_duration()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_motion_for_pouring")
+def get_motion_for_pouring():
+    response = NEEMData().get_motion_for_pouring()
+    if response is not None:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 400
+
+
+@app.route("/get_hand_used_for_pouring")
+def get_hand_used_for_pouring():
+    response = NEEMData().get_hand_used_for_pouring()
     if response is not None:
         return jsonify(response), 200
     else:
--- src/rest_neem_interface/neemdata.py
+++ src/rest_neem_interface/neemdata.py
@@ -7,7 +7,7 @@ import time
 from src.neem_interface_python.rosprolog_client import Prolog, atom
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-neem_uri = '/home/avyas/catkin_ws/src/pouring_hands_neem/NEEM-1'
+neem_uri = '/home/avyas/catkin_ws/src/pouring_apartment_neem/NEEM'
 
 class NEEMData(object):
     """
@@ -44,15 +44,102 @@ class NEEMData(object):
         print(response)
         return response
 
+    def get_all_actions_end_timestamps(self):
+        # prolog exception will be raised if response is none
+        response = self.prolog.ensure_once("findall([End, Evt], event_interval(Evt, _, End), EndTimes)")
+        print(response)
+        return response
+
     def get_all_objects_participates_in_actions(self):
         # prolog exception will be raised if response is none
         response = self.prolog.ensure_once("findall([Act, Obj], has_participant(Act, Obj), Obj)")
         return response
-    
+
     def get_handpose_at_start_of_action(self):
         # prolog exception will be raised if response is none
         # TODO: This call has bug from knowrob side, fix it. Call Human hand once bug is fixed 
         response = self.prolog.once("executes_task(Action, Task),has_type(Task, soma:'Grasping'),event_interval(Action, Start, End),time_scope(Start, End, QScope),tf:tf_get_pose('http://knowrob.org/kb/pouring_hands_map.owl#right_hand_1', [map, Pose, Rotation], QScope,_)")
+        print("response with poses: ", response)
+        return response
+
+    def get_source_container_while_grasping(self):
+        # prolog exception will be raised if response is none
+        response = self.prolog.ensure_once("has_type(Tsk, soma:'Grasping'),executes_task(Act, Tsk), has_participant(Act, Obj), has_type(Role, soma:'SourceContainer'), triple(Obj, dul:'hasRole', Role)")
+        return response
+
+    def get_source_container_pose_while_grasping(self):
+        # prolog exception will be raised if response is none
+        # TODO: This call has bug from knowrob side, fix it. Call Human hand once bug is fixed 
+        response = self.prolog.once("has_type(Tsk, soma:'Grasping'),executes_task(Act, Tsk), has_participant(Act, Obj),event_interval(Act, Start, End),time_scope(Start, End, QScope),tf:tf_get_pose(Obj, [map, Pose, Rotation], QScope,_)")
+        print("response with poses: ", response)
+        return response
+
+    def get_all_obj_roles_which_participate_each_event(self):
+        # prolog exception will be raised if response is none
+        # TODO: This call has bug from knowrob side, fix it. Call Human hand once bug is fixed 
+        response = self.prolog.once("findall([Act, Obj, ObjType, Role], (has_participant(Act, Obj), has_type(Obj, ObjType), triple(Obj, dul:'hasRole', Role)) , Obj)")
+        print("response with poses: ", response)
+        return response
+    
+    def get_shape_for_source_container_objects(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, soma:'Grasping'),executes_task(Act, Tsk), has_participant(Act, Obj), "
+                                    "has_type(Role, soma:'SourceContainer'),triple(Obj, dul:'hasRole', Role), triple(Obj, soma:'hasShape', Shape), has_region(Shape, ShapeRegion)")
+        
+        print("response with poses: ", response)
+        return response
+
+
+    def get_color_for_source_container_objects(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, soma:'Grasping'),executes_task(Act, Tsk), has_participant(Act, Obj), has_type(Role, soma:'SourceContainer'),"
+                                    "triple(Obj, dul:'hasRole', Role), triple(Obj, soma:'hasColor', Color), has_region(Color, ColorRegion)")
+
+        print("response with poses: ", response)
+        return response
+
+
+    def get_target_obj_for_pouring(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, 'http://www.ease-crc.org/ont/SOMA-ACT.owl#Pouring'),executes_task(Act, Tsk), has_participant(Act, Obj), has_type(Role, soma:'DestinationContainer'), triple(Obj, dul:'hasRole', Role)")
+        print("response with poses: ", response)
+        return response
+
+    def get_pouring_side(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, 'http://www.ease-crc.org/ont/SOMA-ACT.owl#Pouring'),executes_task(Act, Tsk), has_participant(Act, Obj), "
+                                    "triple(Obj, dul:'hasLocation', Location)")
+        print("response with poses: ", response)
+        return response
+
+    def get_max_pouring_angle_for_source_obj(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("triple(Individual, 'http://www.ease-crc.org/ont/SOMA-OBJ.owl#hasJointPositionMax', AngleValue)")
+        print("response with poses: ", response)
+        return response
+
+    def get_min_pouring_angle_for_source_obj(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("triple(Individual, 'http://www.ease-crc.org/ont/SOMA-OBJ.owl#hasJointPositionMin', AngleValue)")
+        print("response with poses: ", response)
+        return response
+    
+    def get_pouring_event_time_duration(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, 'http://www.ease-crc.org/ont/SOMA-ACT.owl#Pouring'),executes_task(Act, Tsk), event_interval(Act, Begin, End)")
+        print("response with poses: ", response)
+        return response
+    
+    def get_motion_for_pouring(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, 'http://www.ease-crc.org/ont/SOMA-ACT.owl#Pouring'),executes_task(Act, Tsk), "
+                                    "triple(Motion, dul:'classifies', Act), triple(Motion,dul:'isClassifiedBy', Role), triple(Obj, dul:'hasRole', Role)")
+        print("response with poses: ", response)
+        return response
+        
+    def get_hand_used_for_pouring(self):
+        # prolog exception will be raised if response is none 
+        response = self.prolog.once("has_type(Tsk, 'http://www.ease-crc.org/ont/SOMA-ACT.owl#Pouring'),executes_task(Act, Tsk), has_type(Hand, soma:'Hand')")
         print("response with poses: ", response)
         return response
     
